7ba233c637d156bc795bc600b84ef1530ab5f9b8
Achmat Toffa <achmat.toffa@gmail.com>
2022-05-23 14:47:09 +0200

    Update entrypoint.sh

--- sources/inverter-mqtt/entrypoint.sh
+++ sources/inverter-mqtt/entrypoint.sh
@@ -2,4 +2,4 @@
 export TERM=xterm
 
 # execute exactly every 15 seconds...
-watch -n 15 /opt/inverter-mqtt/mqtt-push.sh > /dev/null 2>&1 &
+watch -n 15 /opt/inverter-mqtt/mqtt-push.sh > /dev/null 2>&1
